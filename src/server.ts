c21d62698a878e91a8c5e2532ec10e6eb48a9a96
Daniel Lucas <danielluccas000@gmail.com>
2024-03-08 08:20:38 -0300

    feat(graphql) adicionado HelloResolver

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,22 @@
-interface User {
-  name: string;
-  email: string;
+import 'reflect-metadata';
+import path from 'path';
+import { buildSchema } from 'type-graphql';
+import { ApolloServer } from 'apollo-server';
+import { HelloResolver } from './resolvers/HelloResolver';
+
+async function main() {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+    emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  const { url } = await server.listen();
+
+  console.log(`Server runing on ${url}`);
 }
 
-function saveUserToDatabase(user: User) {
-  console.log(user);
-  console.log('Branch develop');
-}
-
-saveUserToDatabase({
-  name: 'Daniel Lucas',
-  email: 'daniel@exemple.com',
-});
+main();
